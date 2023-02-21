090f80010ad42f0ac807cb6afc23858e18a74aca
Hossam Moussa <hossam2530@gmail.com>
2023-02-21 22:32:15 +0300

    remove bin from gitignore

---
+++ udagram-api/bin/initEnv.sh
@@ -0,0 +1,3 @@
+eb list
+eb use udagram-api-dev
+eb setenv POSTGRES_USERNAME=$userName POSTGRES_PASSWORD=$password POSTGRES_DB=$dbName POSTGRES_HOST=$dbHost POSTGRES_PORT=$dbPort JWT_SECRET=$jwtSecret SERVER_PORT=$serverPort
