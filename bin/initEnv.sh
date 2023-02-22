949068aa0a9fbcc03326d23560ad81c5a37a5f90
Hossam Moussa <hossam2530@gmail.com>
2023-02-22 21:49:22 +0300

    add aws env

--- bin/initEnv.sh
+++ bin/initEnv.sh
@@ -1,5 +1,5 @@
 cd udagram-api
 eb list
 eb use udagram-api-dev
-eb setenv POSTGRES_USERNAME=$userName POSTGRES_PASSWORD=$password POSTGRES_DB=$dbName POSTGRES_HOST=$dbHost POSTGRES_PORT=$dbPort JWT_SECRET=$jwtSecret SERVER_PORT=$serverPort
+eb setenv POSTGRES_USERNAME=$userName POSTGRES_PASSWORD=$password POSTGRES_DB=$dbName POSTGRES_HOST=$dbHost POSTGRES_PORT=$dbPort JWT_SECRET=$jwtSecret SERVER_PORT=$serverPort AWS_REGION=$awsRegion AWS_PROFILE=$awsProfile AWS_BUCKET=$awsBucket URL=$URL
 cd ..
