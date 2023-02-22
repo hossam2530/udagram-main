6ddf8553f9fee4d885ad2aa315ddc745d78892f5
Hossam Moussa <hossam2530@gmail.com>
2023-02-22 16:30:02 +0300

    fix for env script

--- bin/initEnv.sh
+++ bin/initEnv.sh
@@ -1,4 +1,5 @@
-bash 
+cd udagram-api
 eb list
 eb use udagram-api-dev
 eb setenv POSTGRES_USERNAME=$userName POSTGRES_PASSWORD=$password POSTGRES_DB=$dbName POSTGRES_HOST=$dbHost POSTGRES_PORT=$dbPort JWT_SECRET=$jwtSecret SERVER_PORT=$serverPort
+cd ..
