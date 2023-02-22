79e3811c5934167f58dfc11aaf07af3a3a05446f
Hossam Moussa <hossam2530@gmail.com>
2023-02-22 14:52:34 +0300

    complete circleci yml

--- bin/initEnv.sh
+++ bin/initEnv.sh
@@ -1,3 +1,4 @@
+bash 
 eb list
 eb use udagram-api-dev
 eb setenv POSTGRES_USERNAME=$userName POSTGRES_PASSWORD=$password POSTGRES_DB=$dbName POSTGRES_HOST=$dbHost POSTGRES_PORT=$dbPort JWT_SECRET=$jwtSecret SERVER_PORT=$serverPort
